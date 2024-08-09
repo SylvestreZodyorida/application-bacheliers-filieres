44023ea3ad467af803ae35ffd3b440ff9f4e8db7
SylvestreZodyorida <sylvestreahlonsou88@gmail.com>
2024-08-09 06:54:14 +0100

    Migrations created

---
+++ backend/database/migrations/2024_08_09_054542_create_students_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->enum('entry_mode', ['classement', 'concours']);
+            $table->foreignId('series_id')->constrained('series')->onDelete('cascade');
+            $table->float('average')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
